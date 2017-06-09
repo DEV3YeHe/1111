cd772dd0836c057e6ed3f77d742c7504abb39c1d
DavidYoXiong <422923100@qq.com>
2017-06-09 16:46:36 +0800

    backup
    刚弄坏了框架，吓死；已恢复，赶紧保存。

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import styles from './Rating.css';
 import { TweenMax, TimelineMax } from "gsap";
+import { Button, Input } from 'antd';
+import QueueAnim from 'rc-queue-anim';
+
+
 
 class Rating extends React.Component {
   componentDidMount() {
@@ -10,7 +14,7 @@ class Rating extends React.Component {
   },
   list_obj = document.getElementsByName("hahastar"),
   list_spark = document.getElementsByName("hahaspark"),
-  // list_path = document.getElementsByTagName("path"),
+  rateWin = select('#rateWin'),
   s01 = select('#s01'),
   s02 = select('#s02'),
   s03 = select('#s03'),
@@ -20,103 +24,171 @@ class Rating extends React.Component {
   s12 = select('#s12'),
   s13 = select('#s13'),
   s14 = select('#s14'),
-  s15 = select('#s15')
-
+  s15 = select('#s15'),
+  ipt = select('#ipt'),
+  ipt1 = select('#ipt1')
 //——飞入！！！！
   for (let i = 0; i <= list_obj.length; i++) {
     
     let st1 = new TimelineMax({delay:( 1 + 0.1 * i )});          //TimelineMax 一定要传输组
     st1.from([list_obj[i]], 0.5, {opacity:0,scale:0.2,top:"-18px",rotation:"-420deg",left:"-20px",ease:"Circ.easeOut"})
-    .to([list_obj[i]], 0.2, {scale:1.2} )
-    .to([list_obj[i]], 0.1, {scale:1.0} );
+    .to([list_obj[i]], 0.2, {scale:1.2,fill:"#FDDD9B"} )
+    .to([list_obj[i]], 0.1, {scale:1.0,fill:"#eee"} );
   }
 //——飞入！！！！
 //——————————————————————————————————————————————————
 
 //——将 list_obj 从对象集合转为数组，并测试
-  // alert(list_obj) 
   list_obj = Array.prototype.slice.call(list_obj); 
   list_spark = Array.prototype.slice.call(list_spark);
-  // alert(list_obj instanceof Array); 
-  // alert(list_obj) 
-  console.log(list_path)
+
+  let sparkPath = [];
+  for (let i = 0; i <= 4; i++) {
+    sparkPath[i] = list_spark[i].childNodes
+    sparkPath[i] = Array.prototype.slice.call(sparkPath[i]);
+  }
 //——将 list_obj 从对象集合转为数组，并测试
 //——————————————————————————————————————————————————
 
 //——搞 hover 效果 + 点击效果
-  
-  // let j =0;   //储存出发时间的是第几个星星
-  // let h =0;   //点击的是第几个
-  // let list_onStar = [];   //空数组 用于存可变的星星数
-  let c =0;
+  let c =1;
 
   const overstar = (e) => {               //hover 上
+          // console.log(e.path[1])
+      e.cancelBubble=true;
+      e.stopPropagation();
+      e.bubble=false;
+      // console.log(e)
+      // console.log(e.target.id)
     if(e.target.tagName == "use"){        //鼠标指向svg中的use时，算是出了svg，所以这里用hover use来触发hover；确定 hover 到 use 上了。
-      let j =list_obj.indexOf(event.path[1])  //得到 hover 的第几个
+      // let obj = e.path[1]
+      // console.log(e.target.id)
+      let zifu = e.target.id
+
+      let j =zifu.substring(2)  //得到 hover 的第几个
       // list_onStar = list_obj.slice(0, j++)     //取到需要变化的星星
       // TweenMax.to(list_onStar, 0.1, { scale:1.3, fill:"#FDDD9B"}) //实施变化
-      for (let i = 0; i <= j; i++) {
-        TweenMax.to(list_obj[i], 0.1, { scale:1.3, fill:"#FDDD9B"})
+      for (let i = 0; i < j; i++) {
+        TweenMax.to(list_obj[i], 0.5, { fill:"#FDDD9B"})
       }  
     }
   }
   const outstar = (e) => {                //hover out
     if(e.target.tagName == "use"){  
-      let j =list_obj.indexOf(event.path[1]) 
-      for (let i = 0; i <= j; i++) {
+      let zifu = e.target.id
+      let j =zifu.substring(2)
+      for (let i = 0; i < j; i++) {
         TweenMax.to(list_obj[i], 0.1, { scale:1, fill:"#eee"})
       }
     }
   }
+
   const overaction = (e) =>{               //循环解除事件绑定#FF5500
     let h = 4;
+    sta();
     if(e.target.tagName == "use"){        //鼠标指向svg中的use时，算是出了svg，所以这里用hover use来触发hover；确定 hover 到 use 上了。
-      h =list_obj.indexOf(event.path[1])
+        let zifu = e.target.id;            //取出use 的 id
+        let q =zifu.substring(2);          //id换成 第几个
+        q = parseInt(q);
+        let h = q - 1;
+            for (let j = 0; j <= 3; j++) {           // 放射
+                let segment = new Segment(sparkPath[h][j]);
+                let st4 = new TimelineMax();
 
-      // let col = `rgb(${(155 * 0.2 * (h+1)) + 100},100,100)`    //变化颜色
-      // console.log(col)
-      // console.log(h)
-      let st2 = new TimelineMax();
-      st2.to([list_obj[h]], 0.07, { scale:0.8,rotation:"-72deg", fill:"#FFEEE6"})
-      .to([list_obj[h]], 0.07, { scale:1.4, fill:"#FF5500"})
-      .to([list_obj[h]], 0.8, { scale:1.3,rotation:"288deg", fill:"#FDDD9B",ease:"Circ.easeOut"})
+                st4.to([sparkPath[h][j]], 0, { opacity:1 })
+                .fromTo([sparkPath[h][j]], 0.5, { stroke: '#FF5500',strokeDasharray: segment.strokeDasharray(0, 10) }, { stroke: '#FFEEE6',strokeDasharray: segment.strokeDasharray(100, 100) })
+                .to([sparkPath[h][j]], 0.07, { opacity:0 })
+            };
 
-      if(c){
-        c=0
-        for (let i = 0; i <= 4; i++) {
-          if(list_obj[i]){
-              list_obj[i].removeEventListener('mouseover', overstar,true)
-              list_obj[i].removeEventListener('mouseout', outstar,true)
-              // console.log('remove'+'   '+i+'   '+c)
-          }
-        }
-      }else{
-        // c=1
-        // console.log(h)
-        for (let i = 0; i <= 4; i++) {
-          if(list_obj[i]){
-            // list_obj[i].addEventListener('mouseover', overstar,true)
-            // list_obj[i].addEventListener('mouseout', outstar,true)
-            TweenMax.to(list_obj[i], 0, { scale:1, fill:"#eee"})
-          }
-        }
-        for (let i = 0; i <= h; i++) {
-          if(list_obj[i]){
-            // list_obj[i].addEventListener('mouseover', overstar,true)
-            // list_obj[i].addEventListener('mouseout', outstar,true)
-            TweenMax.to(list_obj[i], 0, { scale:1.3, fill:"#FDDD9B"})
-          }
-        }
+      let st5 = new TimelineMax();                    //点击的星星 high
+      st5.to([list_obj[h]], 0.07, { scale:1.5, fill:"#FFEEE6",rotation:"0deg"})
+      .to([list_obj[h]], 0.07, { fill:"#FF5500"})
+      .to([list_obj[h]], 0.8, { scale:1.3,rotation:"288deg", fill:"#FDDD9B",ease:"Circ.easeOut"});
+
+            if(c){
+                c=0;
+                for (let j = 0; j < h; j++)  {
+                    TweenMax.to(list_obj[j], 0, { scale:1.3, fill:"#FDDD9B"})
+                }
+
+                for (let i = 0; i <= 4; i++) {
+                  if(list_obj[i]){
+                      list_obj[i].removeEventListener('mouseover', overstar,true)
+                      list_obj[i].removeEventListener('mouseout', outstar,true)
+                  }
+                }
+            }else{
+              for (let i = 0; i <= 4; i++) {
+                if(list_obj[i]){
+                  TweenMax.to(list_obj[i], 0, { scale:1, fill:"#eee"})
+                }
+              }
+              for (let i = 0; i <= h; i++) {
+                if(list_obj[i]){
+                  TweenMax.to(list_obj[i], 0, { scale:1.3, fill:"#FDDD9B"})
+                }
+              }
+            }
       }
     }
-  }
+  const overactiont = (e) =>{               //循环解除事件绑定#FF5500
 
+    e.preventDefault();
+    let h = 4;
+
+    if(e.target.tagName == "use"){        //鼠标指向svg中的use时，算是出了svg，所以这里用hover use来触发hover；确定 hover 到 use 上了。
+        let zifu = e.target.id;            //取出use 的 id
+        let q =zifu.substring(2);          //id换成 第几个
+        q = parseInt(q);
+        let h = q - 1;
+            for (let j = 0; j <= 3; j++) {           // 放射
+                let segment = new Segment(sparkPath[h][j]);
+                let st4 = new TimelineMax();
+
+                st4.to([sparkPath[h][j]], 0, { opacity:1 })
+                .fromTo([sparkPath[h][j]], 0.5, { stroke: '#FF5500',strokeDasharray: segment.strokeDasharray(0, 10) }, { stroke: '#FFEEE6',strokeDasharray: segment.strokeDasharray(100, 100) })
+                .to([sparkPath[h][j]], 0.07, { opacity:0 })
+            };
+
+      let st5 = new TimelineMax();                    //点击的星星 high
+      st5.to([list_obj[h]], 0.07, { scale:1.5, fill:"#FFEEE6",rotation:"0deg"})
+      .to([list_obj[h]], 0.07, { fill:"#FF5500"})
+      .to([list_obj[h]], 0.8, { scale:1.3,rotation:"288deg", fill:"#FDDD9B",ease:"Circ.easeOut"});
+
+            if(c){
+                c=0;
+                for (let j = 0; j < h; j++)  {
+                    TweenMax.to(list_obj[j], 0, { scale:1.3, fill:"#FDDD9B"})
+                }
+
+                for (let i = 0; i <= 4; i++) {
+                  if(list_obj[i]){
+                      list_obj[i].removeEventListener('mouseover', overstar,true)
+                      list_obj[i].removeEventListener('mouseout', outstar,true)
+                  }
+                }
+            }else{
+              for (let i = 0; i <= 4; i++) {
+                if(list_obj[i]){
+                  TweenMax.to(list_obj[i], 0, { scale:1, fill:"#eee"})
+                }
+              }
+              for (let i = 0; i <= h; i++) {
+                if(list_obj[i]){
+                  TweenMax.to(list_obj[i], 0, { scale:1.3, fill:"#FDDD9B"})
+                }
+              }
+            }
+      }
+    }
   for (let i = 0; i < list_obj.length; i++) {
     if(list_obj[i]){
       list_obj[i].addEventListener('mouseover', overstar,true)
       list_obj[i].addEventListener('mouseout', outstar,true)
-      list_obj[i].addEventListener('click', overaction,false)
-      c=1
+      list_obj[i].addEventListener('click', overaction,true)
+      list_obj[i].addEventListener('touchend', overactiont,true)
+// TweenMax.to(list_obj[4], 0, { scale:1.3, fill:"#000"}).delay(6) 
+      // c=1
     }
   }
 //——搞 hover 效果 + 点击效果
@@ -155,7 +227,16 @@ class Rating extends React.Component {
   // }
   // bindaction();
 //第一版————————————————————————————————————————
-
+  // TweenMax.set(ipt, {opacity:0})
+  // TweenMax.set(ipt1, {opacity:0})
+  function sta(){
+    console.log("123")
+    TweenMax.to(rateWin, 1.4, { height: '380px' }).delay(1.5)
+    TweenMax.to(rateWin, 1, { height: '380px' })
+  }
+  
+  // TweenMax.from(ipt, 2, { opacity:0 }).delay(4)
+  // TweenMax.from(ipt1, 2, { opacity:0 }).delay(3)
   }
 
   componentWillUnmount() {
@@ -164,44 +245,45 @@ class Rating extends React.Component {
   
   render() {
 
+
     return (
       <div id="rateWin" className={styles.rateWin}>
           <div>
-            <p style={{color:'rgba(0, 0, 0, 0.68)',margin:'30px 0 30px 0',fontSize:'1.17em',fontWeight: '400px',width:'100%',textAlign:'center'}}>请对我的服务做出评价</p>
+            <p style={{color:'rgba(0, 0, 0, 0.68)',margin:'30px 0 30px 0',fontSize:'1.2em',fontWeight: '400px',width:'100%',textAlign:'center'}}>请对服务做出评价哦~</p>
           </div>
 
-                    <div className={styles.sparks}>
+            <div className={styles.sparks}>
 
             <div id="iconWrapper" className={styles.iconWrapper}>
               <svg className={styles.sparkSvg} viewBox="0 0 1600 1600" id='s01' name="hahaspark">
-                <path id="pathA" d="M266 527 L62 428"/>
-                <path id="pathB" d="M359 1189 L83 1441"/>
-                <path id="pathC" d="M1330 1130 L1538 1293"/>
-                <path id="pathD" d="M1204 391 L1453 130"/>
+                <path d="M266 527 L62 428"/>
+                <path d="M359 1189 L83 1441"/>
+                <path d="M1330 1130 L1538 1293"/>
+                <path d="M1204 391 L1453 130"/>
               </svg>
               <svg className={styles.sparkSvg} viewBox="0 0 1600 1600" id='s02' name="hahaspark">
-                <path id="pathA" d="M266 527 L62 428"/>
-                <path id="pathB" d="M359 1189 L83 1441"/>
-                <path id="pathC" d="M1330 1130 L1538 1293"/>
-                <path id="pathD" d="M1204 391 L1453 130"/>
+                <path d="M266 527 L62 428"/>
+                <path d="M359 1189 L83 1441"/>
+                <path d="M1330 1130 L1538 1293"/>
+                <path d="M1204 391 L1453 130"/>
               </svg>
               <svg className={styles.sparkSvg} viewBox="0 0 1600 1600" id='s03' name="hahaspark">
-                <path id="pathA" d="M266 527 L62 428"/>
-                <path id="pathB" d="M359 1189 L83 1441"/>
-                <path id="pathC" d="M1330 1130 L1538 1293"/>
-                <path id="pathD" d="M1204 391 L1453 130"/>
+                <path d="M266 527 L62 428"/>
+                <path d="M359 1189 L83 1441"/>
+                <path d="M1330 1130 L1538 1293"/>
+                <path d="M1204 391 L1453 130"/>
               </svg>
               <svg className={styles.sparkSvg} viewBox="0 0 1600 1600" id='s04' name="hahaspark">
-                <path id="pathA" d="M266 527 L62 428"/>
-                <path id="pathB" d="M359 1189 L83 1441"/>
-                <path id="pathC" d="M1330 1130 L1538 1293"/>
-                <path id="pathD" d="M1204 391 L1453 130"/>
+                <path d="M266 527 L62 428"/>
+                <path d="M359 1189 L83 1441"/>
+                <path d="M1330 1130 L1538 1293"/>
+                <path d="M1204 391 L1453 130"/>
               </svg>
               <svg className={styles.sparkSvg} viewBox="0 0 1600 1600" id='s05' name="hahaspark">
-                <path id="pathA" d="M266 527 L62 428"/>
-                <path id="pathB" d="M359 1189 L83 1441"/>
-                <path id="pathC" d="M1330 1130 L1538 1293"/>
-                <path id="pathD" d="M1204 391 L1453 130"/>
+                <path d="M266 527 L62 428"/>
+                <path d="M359 1189 L83 1441"/>
+                <path d="M1330 1130 L1538 1293"/>
+                <path d="M1204 391 L1453 130"/>
               </svg>
             </div>
             
@@ -211,24 +293,46 @@ class Rating extends React.Component {
               
               <div id='hoverstars' className={styles.hoverstars}>
                   <svg className={styles.hoverstar} id='s11' name="hahastar">
-                    <use xlinkHref="#icon-star"></use>
+                    <use xlinkHref="#icon-star" id='u11'></use>
                   </svg>
                   <svg className={styles.hoverstar} id='s12' name="hahastar">
-                    <use xlinkHref="#icon-star"></use>
+                    <use xlinkHref="#icon-star" id='u12'></use>
                   </svg>
                   <svg className={styles.hoverstar} id='s13' name="hahastar">
-                    <use xlinkHref="#icon-star"></use>
+                    <use xlinkHref="#icon-star" id='u13'></use>
                   </svg>
                   <svg className={styles.hoverstar} id='s14' name="hahastar">
-                    <use xlinkHref="#icon-star"></use>
+                    <use xlinkHref="#icon-star" id='u14'></use>
                   </svg>
                   <svg className={styles.hoverstar} id='s15' name="hahastar">
-                    <use xlinkHref="#icon-star"></use>
+                    <use xlinkHref="#icon-star" id='u15'></use>
                   </svg>
               </div>
 
           </div>
 
+          <QueueAnim className={styles.normal} animConfig={[
+              { opacity: [1, 0], translateY: [0, 90] },
+              { opacity: [1, 0], translateY: [0, -90] }
+            ]} delay={1160} duration={700}>
+            {true ? [ 
+
+          <div className={styles.ipt} key='1'>
+            {/* <div className={styles.cover} id='cover'></div> */}
+            
+
+            <Input id='ipt' type="textarea" autosize={{ minRows: 5, maxRows: 5 }}></Input>
+
+          
+            <Button id='ipt1' style={{marginTop:'35px',width: '100%'}} size="large" type="primary" >
+            Submit</Button>
+            
+
+          </div>
+
+            ]: null}
+          </QueueAnim>
+          
           
       </div>
     )
--- src/routes/Dashboard.js
+++ src/routes/Dashboard.js
@@ -3,19 +3,29 @@ import { connect } from 'dva';
 import styles from './Dashboard.css';
 import Bear from '../components/Bear';
 import Rating from '../components/Rating';
+import QueueAnim from 'rc-queue-anim';
 
 function Dashboard() {
+
   return (
     <div className={styles.normal}>
-	    
-		<Rating/>
+	    <QueueAnim className={styles.normal} animConfig={[
+            { opacity: [1, 0], translateY: [0, 90] },
+            { opacity: [1, 0], translateY: [0, -90] }
+          ]} delay={60} duration={700}>
+          {true ? [
 
+		<Rating key='1'/>
+		
+		]: null}
+        </QueueAnim>
     </div>
   );
 }
 
-function mapStateToProps() {
-  return {};
+function mapStateToProps(state) {
+  return {
+  };
 }
 
 export default connect(mapStateToProps)(Dashboard);
